919ea25c187e67475278d80dcff96f1a225e72ad
rajutamanglama65 <raju@fusemachines.com>
2023-04-25 16:37:14 +0545

    feat: navbar work on progress

--- src/components/nav/Nav.styles.ts
+++ src/components/nav/Nav.styles.ts
@@ -1,9 +1,26 @@
 import styled from 'styled-components'
 
 export const StyledNavbar = styled.div`
-    height: 2rem;
+    // height: 3rem;
     width: 100%;
-    border: 2px solid #4caf50;
-    background-color: #4caf50;
+    // border: 2px solid #E58799;
+    position: fixed;
+    z-index: 50;
+    display: flex;
+    align-items: center;
 `
 
+export const NavContainer = styled.div`
+    margin: 0 auto;
+    width: 50%;
+    height: 3rem;
+    // border: 1px solid pink;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`
+
+export const Logo = styled.h2`
+    letter-spacing: .2rem;
+    color: #E58799;
+`
--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import { StyledNavbar } from './Nav.styles'
+import {  XMarkIcon, ShoppingCartIcon } from "@heroicons/react/24/solid";
+import { Logo, NavContainer, StyledNavbar } from './Nav.styles'
 
 type Props = {}
 
 const Navbar = (props: Props) => {
   return (
-    <StyledNavbar>Here is nav bar.</StyledNavbar>
+    <StyledNavbar>
+      <NavContainer>
+          <Logo>Shopping Cart</Logo>
+          <ShoppingCartIcon style={{height: "2rem", width: "2rem", color: "#e66465"}} />
+      </NavContainer>
+    </StyledNavbar>
   )
 }
 
