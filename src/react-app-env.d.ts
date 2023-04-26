905fd02cf33beef91eb35c96b013aa4dcb03d2de
rajutamanglama65 <raju@fusemachines.com>
2023-04-26 14:52:55 +0545

    feat: fetch fake data to use

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="react-scripts" />
+
+export type CartItemType = {
+    id: number;
+    category: string;
+    description: string;
+    image: string;
+    price: number;
+    title: string;
+    amount: number;
+}
--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { useQuery } from 'react-query'
+import { CartItemType } from '../../react-app-env'
 import { RootDiv } from './Home.styles'
 
 type Props = {}
 
+const getProducts = async (): Promise<CartItemType[]> => 
+    await (await fetch('https://fakestoreapi.com/products')).json()
+
+
 const Home = (props: Props) => {
+  const {data, isLoading, error} = useQuery<CartItemType[]>('products', getProducts)
+  console.log("data: ", data)
+  
   return (
     <RootDiv>This is Home screen.</RootDiv>
   )
