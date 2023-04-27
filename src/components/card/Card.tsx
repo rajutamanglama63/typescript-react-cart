e1a68eb2e271074a628bf39c2849e99100aacac7
rajutamanglama65 <raju@fusemachines.com>
2023-04-27 16:54:14 +0545

    feat: fetched data rendered

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,23 +1,22 @@
 import { FC } from 'react'
+import { ProductContextType } from '../../react-app-env';
 import { AddBtn, Description, Image, Price, StyledCard, Title } from './Card.styles'
 
 
-type Props = {}
+type DatumType = {
+  
+  datum: ProductContextType
+    
+  
+}
 
-const Card: FC = (props: Props) => {
+const Card: FC<DatumType> = ({datum}) => {
   return (
     <StyledCard>
-        <Image alt='product-img' src='https://images.unsplash.com/photo-1614008262085-fad18b3eeef6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y2FtYXJhfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60' />
+        <Image alt='product-img' src={datum.image} />
         <Title>Camara</Title>
-        <Price><span style={{"fontWeight": "700"}}>Price</span>: Rs 123</Price>
-        <Description>Lorem Ipsum is simply dummy text 
-          of the printing and typesetting industry. 
-          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-          when an unknown printer took a galley of type and 
-          scrambled it to make a type specimen book. It has survived not only 
-          five centuries, but also the leap into electronic typesetting, 
-          remaining essentially unchanged
-        </Description>
+        <Price><span style={{"fontWeight": "700"}}>Price</span>: Rs {datum.price}</Price>
+        <Description>{datum.description}</Description>
         <AddBtn variant='outline'>Add To Cart</AddBtn>
     </StyledCard>
   )
--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -15,7 +15,7 @@ const Home = (props: Props) => {
 
   const getProducts = useContext(ProductContext)
   const {data, isLoading, error} = useQuery<CartItemType[]>('products', getProducts)
-  console.log("data: ", data)
+  // console.log("data: ", data)
 
   // if(isLoading) return <h1>Loading...</h1>
 
@@ -24,13 +24,9 @@ const Home = (props: Props) => {
   return (
     <RootDiv>
       <HomeContainer>
+        {/* {isLoading ? (<>Loading...</>) ? error ? (<>Something went wrong...</>) : data?.map((datum) => (<Card id={datum.id} datum={datum} />))}; */}
         
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {data?.map((datum) => (<Card key={datum.id} datum={datum} />))}
       </HomeContainer>
     </RootDiv>
   )
